45be529406e3383c3187eb1836ba7a2475f253a7
Amaury Bréhéret <abreheret@gmail.com>
2017-01-18 09:25:23 +0100

    clean code add image result

--- src/visodo.cpp
+++ src/visodo.cpp
@@ -42,7 +42,7 @@ THE SOFTWARE.
 #include <fstream>
 #include <string>
 
-#define CV3 1
+#define CV3 0
 #define USE_CUDA 0
 
 using namespace cv;
@@ -92,6 +92,7 @@ int main_test()
     //Recover essential
     cv::Mat E = cv::findEssentialMat(points1, points2, K, cv::FM_RANSAC, 0.99, 1);
     cv::Mat F = cv::findFundamentalMat(points1, points2, cv::FM_RANSAC, 1, 0.9);
+    F = K.t() * F * K;
 
     std::cout << "E: " << E << std::endl;
     std::cout << "F: " << F << std::endl;
@@ -110,6 +111,7 @@ int main_test()
 
     std::cout << "R5: " << R5 << std::endl;
     std::cout << "t5: " << t5 << std::endl;
+    return 1;
 }
 
 double dist_point(const cv::Point2f & point1,const cv::Point2f & point2) {
@@ -164,35 +166,31 @@ using namespace std;
 //#define PATH "D:/KITTI_VO/02/image_0/"
 #define PATH "../KITTI_00/"
 #define DATASET PATH"%06d.png"
-double getAbsoluteScale(int frame_id, int sequence_id, double z_cal)
-{
-    return 1;
+
+
+std::vector<double> getScale() {
+    std::vector<double> scales;
     string line;
     ifstream myfile (PATH"/00.txt");
+    std::vector<double> times;
     int i = 0;
     double x = 0, y = 0, z = 0;
     double x_prev, y_prev, z_prev;
-    if (myfile.is_open()) {
-        while ((getline(myfile, line)) && (i <= frame_id)) {
-            z_prev = z;
-            x_prev = x;
-            y_prev = y;
-            std::istringstream in(line);
-            // cout << line << '\n';
-            for (int j = 0; j < 12; j++) {
-                in >> z ;
-                if (j == 7) y = z;
-                if (j == 3)  x = z;
-            }
-            i++;
+    while ((getline(myfile, line))) {
+        z_prev = z;
+        x_prev = x;
+        y_prev = y;
+        std::istringstream in(line);
+        for (int j = 0; j < 12; j++) {
+            in >> z ;
+            if (j == 7) y = z;
+            if (j == 3)  x = z;
         }
-        myfile.close();
-    } else {
-        cout << "Unable to open file";
-        return 0;
+        i++;
+        scales.push_back(sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)));
     }
-
-    return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
+    myfile.close();
+    return scales;
 }
 
 std::vector<double> getTimes(const std::string & filename) {
@@ -214,13 +212,6 @@ std::vector<double> getTimes(const std::string & filename) {
     }
     return times;
 }
-
-// #define DATASET "/home/james/image_processing/videostab/bags/c15-41b/%04d.png"
-// Mat K = (Mat_<double>(3, 3) << 564.85980225, 0., 973.51554108, 0., 664.61627197, 424.90181213, 0, 0, 1);
-// #define MAX_FRAME 171
-
-
-
 //R0_rect:
 Mat Rext = (Mat_<double>(3, 3) <<
             9.999239000000e-01 ,9.837760000000e-03,-7.445048000000e-03,
@@ -235,94 +226,44 @@ Mat K = (Mat_<double>(3, 3) <<
          718.8560,        0, 607.1928,
          0       , 718.8560, 185.2157,
          0       ,        0,       1);
-#define MAX_FRAME 325
-//P0: 7.188560000000e+02 0.000000000000e+00 6.071928000000e+02 0.000000000000e+00
-//    0.000000000000e+00 7.188560000000e+02 1.852157000000e+02 0.000000000000e+00
-//    0.000000000000e+00 0.000000000000e+00 1.000000000000e+00 0.000000000000e+00
 int main(int argc, char** argv)
 {
-    main_test();
-    Mat img_1, img_2;
-    Mat R_f, t_f; // the final rotation and tranlation vectors containing the
-
-    ofstream myfile;
-    myfile.open("results1_1.txt");
-
-    double scale = 1.00;
+    //main_test(); return 1;
     char filename1[200];
-    char filename2[200];
     sprintf(filename1, DATASET, 0);
-    sprintf(filename2, DATASET, 1);
     std::vector<double> times = getTimes(PATH"times.txt");
-
-    char text[100];
+    char text[100], filename[256];
     int fontFace = FONT_HERSHEY_PLAIN;
     double fontScale = 1;
     int thickness = 1;
-    cv::Point textOrg(10, 50);
 
-    // read the first two frames from the dataset
+    Mat img_1,img_2;
     Mat img_1_c = imread(filename1);
-    Mat img_2_c = imread(filename2);
-
-    if (!img_1_c.data || !img_2_c.data) {
+    if (!img_1_c.data ) {
         std::cout<< " --(!) Error reading images " << std::endl;
         return -1;
     }
-
-    // we work with grayscale images
     cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
-    cvtColor(img_2_c, img_2, COLOR_BGR2GRAY);
-
-    // feature detection, tracking
-    vector<Point2f> points1, points2;        // vectors to store the coordinates of the feature points
-    featureDetection(img_1, points1);        // detect features in img_1
-    vector<uchar> status;
-    featureTracking(img_1, img_2, points1, points2, status); // track those features to img_2
-
-    // recovering the pose and the essential matrix
-    Mat E, R, t;
-#if CV3
-    E = findEssentialMat(points2, points1, K, RANSAC, 0.999, 1.0);
-#else
-    Mat F = findFundamentalMat(points2, points1, FM_RANSAC, 3., 0.99);
-    E = K.t() * F * K;
-#endif
-    recoverPose(E, points2, points1, K, R, t);
-    cout << R << endl;
-
-    Mat prevImage = img_2;
+    Mat prevImage = img_1;
     Mat currImage;
-    vector<Point2f> prevFeatures = points2;
+    vector<Point2f> prevFeatures;
     vector<Point2f> currFeatures;
-
-    char filename[100];
-
-    R_f = R.clone();
-    t_f = t.clone();
-
-    clock_t begin = clock();
-
-    // namedWindow("Road facing camera", WINDOW_AUTOSIZE);// Create a window for display.
-    // namedWindow("Trajectory", WINDOW_AUTOSIZE);// Create a window for display.
-
+    featureDetection(img_1, prevFeatures);// detect features in img_1
+    Mat R_f = Mat::eye(3,3,CV_64F);
+    Mat t_f = Mat::zeros(3,1,CV_64F);// the final rotation and tranlation vectors containing the
     Mat traj = Mat::zeros(800,800, CV_8UC3);
 
-    double bearing = 0;
-
 #if USE_CUDA
-    CudaFeatureTraker cuda_ft(MIN_NUM_FEAT,21,3,30);
+    CudaFeatureTraker cuda_ft(2*MIN_NUM_FEAT,21,3,30);
 #endif
-    for (int numFrame = 2; numFrame < times.size() ; numFrame++) {
+    std::vector<double> scales = getScale();
+    for (int numFrame = 1; numFrame < times.size() ; numFrame++) {
+        Mat E, R, t;
         sprintf(filename, DATASET, numFrame);
-
         double delta_t =times[numFrame] - times[numFrame-1];
-        //cout << "delta_t = " << delta_t << endl;
         Mat currImage_c = imread(filename);
-
-        if(currImage_c.empty()) {
-            break;
-        }
+        if(currImage_c.empty())
+            break; // error read image
         cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
         vector<uchar> status;
 
@@ -338,92 +279,51 @@ int main(int argc, char** argv)
         Mat F = findFundamentalMat(currFeatures, prevFeatures, FM_RANSAC, 3., 0.99);
         E = K.t() * F * K;
 #endif
-
         recoverPose(E, currFeatures, prevFeatures, K, R, t);
-        //recoverPose(E, currFeatures, prevFeatures, R, t, K.at<double>(0,0), cv::Point(K.at<double>(0,2),K.at<double>(1,2)));
-        cv::Mat euler;
-        Rodrigues(R, euler);
-        //R = trans*Rext*R;
-        //cout << numFrame << " " << 180 * bearing / 3.14159 << endl;
-        Mat prevPts(2, prevFeatures.size(), CV_64F), currPts(2, currFeatures.size(), CV_64F);
-
-        // this (x, y) combination makes sense as observed from the source code of triangulatePoints on GitHub
-        for (int i = 0; i < prevFeatures.size(); i++) {
-            prevPts.at<double>(0, i) = prevFeatures.at(i).x;
-            prevPts.at<double>(1, i) = prevFeatures.at(i).y;
-
-            currPts.at<double>(0, i) = currFeatures.at(i).x;
-            currPts.at<double>(1, i) = currFeatures.at(i).y;
-        }
-
-        scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
-        Mat tt = scale * (R * t) ;
+        Mat tt = scales[numFrame] * (R * t) ;
         double distance = tt.at<double>(2)*tt.at<double>(2)+ tt.at<double>(0)*tt.at<double>(0) + tt.at<double>(1)*tt.at<double>(1);
         distance = sqrt(distance);
-        //cout << "speed :" << t.at<double>(2) /scale << "\n";
         double speed =(3.6*distance/delta_t)  ;
+        cv::Mat euler;
+        Rodrigues(R, euler);
 #define TODEG (180.f/CV_PI)
-        double yaw   = TODEG *euler.at<double>(1)/delta_t;
         double pitch = TODEG *euler.at<double>(0)/delta_t;
+        double yaw   = TODEG *euler.at<double>(1)/delta_t;
         double roll  = TODEG *euler.at<double>(2)/delta_t;
-
-        cout << tt << endl;
-        cout << "speed :"  << speed << " k/h\n"<<
-             "yaw   = " << yaw    <<" deg/s\n" <<
-             "pitch = " << pitch  <<" deg/s\n" <<
-             "roll  = " << roll   <<" deg/s\n" <<
-             "-------------------\n" ;
-
-
-        if ((scale > 0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
-            t_f = t_f + scale * (R_f * t);
+        if ((scales[numFrame] > 0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
+            t_f = t_f + scales[numFrame] * (R_f * t);
             R_f = R * R_f;
         } else {
             cout << "scale below 0.1, or incorrect translation" << endl;
         }
-
-        // lines for printing results
-        // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
-
 #if !USE_CUDA
         // a redetection is triggered in case the number of features being tracked go below a particular threshold
         if (prevFeatures.size() < MIN_NUM_FEAT) {
-            // cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
-            // cout << "trigerring redection" << endl;
             featureDetection(prevImage, prevFeatures);
             featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
         }
 #endif
 
+        // DRAWING ----------------------------------------
         prevImage = currImage.clone();
-
         int x = int(t_f.at<double>(0)) + 300;
         int y = int(t_f.at<double>(2)) + 150;
         circle(traj, Point(x, y), 1, CV_RGB(255, 0, 0), 1);
-
-        rectangle(traj, Point(10, 0), Point(1000, 80), CV_RGB(0, 0, 0), CV_FILLED);
-        sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm speed = %02fkmph",
-                t_f.at<double>(0), t_f.at<double>(1), t_f.at<double>(2),speed);
-        putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness, 8);
-
+        rectangle(traj, Point(0, 0), Point(1000, 100), CV_RGB(0, 0, 0), CV_FILLED);
+        sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm speed = %02fkmph",t_f.at<double>(0), t_f.at<double>(1), t_f.at<double>(2),speed);
+        putText(traj, text, cv::Point(10, 20), fontFace, fontScale, Scalar::all(255), thickness, 8);
+        sprintf(text, "Yaw: %0.2f deg/s pitch = %0.2f deg/s roll = %0.2f deg/s",yaw,pitch,roll);
+        putText(traj, text, cv::Point(10, 40), fontFace, fontScale, Scalar::all(255), thickness, 8);
         for(int i = 0 ; i< currFeatures.size() ; i++) {
             cv::circle(currImage_c,currFeatures[i],2,CV_RGB(255,0,0));
             cv::line(currImage_c,currFeatures[i],prevFeatures[i],CV_RGB(0,0,255));
-
         }
 
         imshow("Road facing camera", currImage_c);
         imshow("Trajectory", traj);
-        waitKey(10);
+        if( waitKey(10) == 27)
+            break;
         prevFeatures = currFeatures;
     }
-
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout << "Total time taken: " << elapsed_secs << "s" << endl;
-
-// cout << R_f << endl;
-// cout << t_f << endl;
-
     return 0;
 }
